ccbfc4a2c852f79f529e9b13bf1ac67ed8e912d0
Orianne Farinelli <orianne.farinelli@epfl.ch>
2026-05-19 20:42:02 +0200

    z offset

--- test/test_lineset_live_shape.py
+++ test/test_lineset_live_shape.py
@@ -391,7 +391,7 @@ class FitWorker:
         
         obj = Float64MultiArray()
         obj.data = [1. if shape == "cylinder" else 0.,
-                    d_m, 0., 0., 0., 0., 0.]
+                    d_m, 0., 0.02, 0., 0., 0.]
         self._node.object_pub.publish(obj)
 
 
